436f897cdf7bfa761a76042d40e968679226209f
Maudien Smittenaar <110417@st-maartenscollege.nl>
2020-11-23 15:29:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def bubbleSort( theSeq ):
+    n = len( theSeq )
+
+    for i in range( n - 1 ) :
+        flag = 0
+
+        for j in range(n - 1) :
+            
+            if theSeq[j] > theSeq[j + 1] : 
+                tmp = theSeq[j]
+                theSeq[j] = theSeq[j + 1]
+                theSeq[j + 1] = tmp
+                flag = 1
+
+        if flag == 0:
+            break
+
+    return theSeq
+
+el = [4,62,39,53,71,12,18,93,21] 
+
+result = bubbleSort(el)
+
+print (result)
